76759c3622118100cbed55d9f9bcb8e71cb68942
arcade_kappa <gkappa@outlands.top>
2026-07-27 09:53:20 +0800

    Fix C23 bool compatibility

--- src/eris.c
+++ src/eris.c
@@ -30,11 +30,14 @@ THE SOFTWARE.
 #include <stdio.h>
 #include <string.h>
 
-/* Not using stdbool because Visual Studio lives in the past... */
 #ifndef __cplusplus
+#if defined(_MSC_VER) && _MSC_VER < 1800
 typedef int bool;
 #define false 0
 #define true 1
+#else
+#include <stdbool.h>
+#endif
 #endif
 
 /* Mark us as part of the Lua core to get access to what we need. */
